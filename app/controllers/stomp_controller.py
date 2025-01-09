f43bf65c54f41a98f2972e18cd07e79a88c54a67
matth <matthieu@lapetitte.fr>
2025-01-09 22:37:47 +0100

    Remove duplicate license comments and update method call
    Eliminated redundant license block comments at the top of the file to improve readability. Updated the `send_message` method call to `send` for compatibility with the current STOMP client API.

--- app/controllers/stomp_controller.py
+++ app/controllers/stomp_controller.py
@@ -10,26 +10,7 @@
 #
 #  Pour toute question ou demande d'autorisation, contactez LAPETITTE Matthieu à l'adresse suivante :
 #  matthieu@lapetitte.fr
-#
-#  Ce fichier est soumis aux termes de la licence suivante :
-#  Vous êtes autorisé à utiliser, modifier et distribuer ce code sous réserve des conditions de la licence.
-#  Vous ne pouvez pas utiliser ce code à des fins commerciales sans autorisation préalable.
-#
-#  Ce fichier est fourni "tel quel", sans garantie d'aucune sorte, expresse ou implicite, y compris mais sans s'y limiter,
-#  les garanties implicites de qualité marchande ou d'adaptation à un usage particulier.
-#
-#  Pour toute question ou demande d'autorisation, contactez LAPETITTE Matthieu à l'adresse suivante :
-#  matthieu@lapetitte.fr
-#
-#  Ce fichier est soumis aux termes de la licence suivante :
-#  Vous êtes autorisé à utiliser, modifier et distribuer ce code sous réserve des conditions de la licence.
-#  Vous ne pouvez pas utiliser ce code à des fins commerciales sans autorisation préalable.
-#
-#  Ce fichier est fourni "tel quel", sans garantie d'aucune sorte, expresse ou implicite, y compris mais sans s'y limiter,
-#  les garanties implicites de qualité marchande ou d'adaptation à un usage particulier.
-#
-#  Pour toute question ou demande d'autorisation, contactez LAPETITTE Matthieu à l'adresse suivante :
-#  matthieu@lapetitte.fr
+
 import os
 import random as rand
 
@@ -116,7 +97,7 @@ class StompMultipleSend:
         self.destination = destination
 
     def send(self, body):
-        self.stomp_client.send_message(self.destination, body, transaction=self.txid)
+        self.stomp_client.send(destination=self.destination, body=body, transaction=self.txid)
 
     def __del__(self):
         logger.info("Stomp : Send all message")
